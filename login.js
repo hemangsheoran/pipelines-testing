7aa9b2339fb54d237e0d44c742f7077251151b98
HEMANG SHEORAN <90271933+hemangsheoran@users.noreply.github.com>
2024-05-24 11:52:03 +0000

    sqlite

--- login.js
+++ login.js
@@ -1,7 +1,9 @@
 touconst fs = require("fs");
 const login = require("fca-jiser-main");
 
-var credentials = {email: "FB_EMAIL", password: "FB_PASSWORD"}; // credential information
+var credentials = {email: "FB_EMAIL", password: "FB_PASSWORD"}; // 
+
+credential information
 
 login(credentials, (err, api) => {
     if(err) return console.error(err);
